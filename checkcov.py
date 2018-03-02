750c0f9b8194fe65d7e44178efd6b19e166bf59e
fai-k <thee.kochakarn@gmail.com>
2018-03-02 19:37:57 +0700

    Create checkcov.py

---
+++ checkcov.py
@@ -0,0 +1,77 @@
+#Check coverage for suspected HRP2/3 deletion
+#Required text file: input-namelocat.txt which contains "isolate name" (tab or space) "file location" in the same line
+#Required: pysam 0.11.2.2, pysamstats 1.0.1
+from __future__ import print_function
+import numpy as np
+import pysam
+import pysamstats as pss
+import csv
+
+#Paths and Parameters#
+inputfile = '/media/xifai/RAVEN/Outfiles5/input-namelocat.txt'
+reffile = '/media/xifai/RAVEN/Ref/Pf3D7_v3.fa'
+cutoff = 0.1	#the normalized coverage cut point for flagging an exon as suspected deletion
+core_win = [['Pf3D7_01_v3',92901,575900],
+	['Pf3D7_02_v3',105801,862500],
+	['Pf3D7_03_v3',70631,1003060],
+	['Pf3D7_09_v3',79101,1473560],
+	['Pf3D7_10_v3',68971,1571815],
+	['Pf3D7_11_v3',110001,2003320],
+	['Pf3D7_13_v3',74414,2791900]]
+exons = [['Pf3D7_08_v3',1358314,1359142,'PF3D7_0831600','Chr8_CLAG8'],
+	['Pf3D7_08_v3',1359287,1359859,'PF3D7_0831600','Chr8_CLAG8'],
+	['Pf3D7_08_v3',1361249,1362015,'PF3D7_0831600','Chr8_CLAG8'],
+	['Pf3D7_08_v3',1362197,1363618,'PF3D7_0831600','Chr8_CLAG8'],
+	['Pf3D7_08_v3',1365467,1367506,'PF3D7_0831700','Chr8_HSP70x'],
+	['Pf3D7_08_v3',1374236,1375084,'PF3D7_0831800','Chr8_HRP2'],
+	['Pf3D7_08_v3',1383349,1384212,'PF3D7_0832000','Chr8_stevor'],
+	['Pf3D7_08_v3',1392407,1393135,'PF3D7_0832200','Chr8_PHISTa-like'],
+	['Pf3D7_13_v3',2833204,2834322,'PF3D7_1372000','Chr13_PHISTa'],
+	['Pf3D7_13_v3',2837313,2839058,'PF3D7_1372100','Chr13_PHISTb'],
+	['Pf3D7_13_v3',2840727,2841485,'PF3D7_1372200','Chr13_HRP3'],
+	['Pf3D7_13_v3',2846002,2846538,'PF3D7_1372300','Chr13_PHIST'],
+	['Pf3D7_13_v3',2850891,2853482,'PF3D7_1372400','Chr13_ACS4']]
+###
+
+samples=[]
+with open(inputfile,'r') as f:
+    for line in csv.reader(f, delimiter='\t'):
+        samples.append(line)
+
+suspectdel=[]
+for i in range(0,len(samples)):
+	bam = pysam.AlignmentFile(samples[i][1],'rb')
+	#Set reference#
+	for j in range(0,len(core_win)):
+		if j == 0:
+			rcoverage = pss.load_coverage_binned(bam, reffile, chrom = core_win[j][0], start = core_win[j][1], end = core_win[j][2])
+		else:
+			rcoverage = np.append(rcoverage,pss.load_coverage_binned(bam, reffile, chrom = core_win[j][0], start = core_win[j][1], end = core_win[j][2]))
+	cov=[]	
+	for q in range(0,len(rcoverage)):
+		if rcoverage[q][2] >= 20:
+			cov.append(rcoverage[q][3])
+	cov_median = np.median(cov)*100/300
+	#Coverage of HRP and neighboring gene#
+	exoncov=[]
+	for k in range(0,len(exons)):
+		ecoverage = pss.load_coverage(bam, reffile, pad = True, truncate = True, chrom = exons[k][0], start = exons[k][1], end = exons[k][2])
+		exonmean = np.mean(ecoverage.reads_all)
+		exoncov.append([exonmean, exonmean/cov_median])
+	with open('HRPcov_%s.txt' % samples[i][0],'w') as f:
+		f.write('chromosome\tstart\tend\tgeneID\tname\tcoverage\tnormalizedcov\n')
+		for m in range(0,len(exons)):
+			f.write('%s\t%s\t%s\t%s\t%s\t%.3f\t%.3f\n' % (exons[m][0], exons[m][1], exons[m][2], exons[m][3], exons[m][4], exoncov[m][0], exoncov[m][1]))
+		f.write('Median coverage: %.3f\n' % cov_median)
+	#Flag suspected deletion#
+	for n in range(0,len(exoncov)):
+		if exoncov[n][1] < cutoff:
+			suspectdel.append(samples[i])
+			break
+	print('Done: %s\n' % samples[i][0])
+with open('suspectdel.txt','w') as f:
+	for p in range(0,len(suspectdel)):
+		f.write('%s\t%s\n' % (suspectdel[p][0], suspectdel[p][1]))
+
+#Normalized coverage of exons in HRP2 and HRP3 neigboring genes are listed in HRPcov_IsolateName.txt
+#Isolate names of the suspected HRP2 or HRP3 deletion are listed in suspectdel.txt
